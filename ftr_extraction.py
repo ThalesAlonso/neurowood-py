7d1d71c245adf3b89ff1b342dcf462bcfbaa88bf
ThalesAlonso <koben_rock@hotmail.com>
2018-03-13 09:15:04 -0300

    Adição dos parametros do LBP

--- ftr_extraction.py
+++ ftr_extraction.py
@@ -1,12 +1,41 @@
+from skimage import feature
+import numpy as np
+
 class FeatureExtractor(object):
     # Construtor
     def __init__(self):
         pass
         
+# Descritor de textura    HaralickExtractor     
 class HaralickExtractor(FeatureExtractor):
     def __init__(self):
         pass
         
+# Descritor de textura LBP 
 class LBPExtractor(FeatureExtractor):
     def __init__(self):
-        pass
+        def __init__(self, numPoints=24, radius=8, method="uniform"):
+		#  armazene o número de pontos e raio
+		self.numPoints = numPoints
+		self.radius = radius
+		self.method = method
+ 
+   def describe(self, image, eps=1e-7):
+	    # computa a representação do padrão binário local
+        # da imagem, e depois use a representação LBP
+        # para construir o histograma de padrões
+		lbp = feature.local_binary_pattern(image, self.numPoints, self.radius, method=self.method)
+		
+		#n_bins = int(lbp.max() + 1)
+        #hist, _ = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
+		(hist, _) = np.histogram(lbp.ravel(),  bins=np.arange(0, self.numPoints + 3), range=(0, self.numPoints + 2))
+		#(hist, _) = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
+ 	  
+		
+        # normalize o histograma
+		hist = hist.astype("float")
+		hist /= (hist.sum() + eps)
+ 
+        # retorna o histograma de Padrões Binários Locais
+		return hist
+        
--- img_processing.py
+++ img_processing.py
@@ -1,3 +1,8 @@
+
+from skimage import data
+import matplotlib.pyplot as plt
+
+
 class Filter(object):
     def __init__(self):
         pass
@@ -10,8 +15,38 @@ class Crop(Operation):
     def __init__(self):
         pass
 
+
 class Gaussian(Filter):
-    def __init__(self):
+    __all__ = ['gaussian']
+    def _convert_input(image, preserve_range):
+    if preserve_range:
+        image = image.astype(np.double)
+    else:
+        image = img_as_float(image)
+    return image
+
+def gaussian(image, sigma=1, output=None, mode='nearest', cval=0,
+             multichannel=None, preserve_range=False, truncate=4.0):
+
+             spatial_dims = guess_spatial_dimensions(image)
+    if spatial_dims is None and multichannel is None:
+        msg = ("Images with dimensions (M, N, 3) are interpreted as 2D+RGB "
+               "by default. Use `multichannel=False` to interpret as "
+               "3D image with last dimension of length 3.")
+        warn(RuntimeWarning(msg))
+        multichannel = True
+    if np.any(np.asarray(sigma) < 0.0):
+        raise ValueError("Sigma Valores inferiores a Zero não são Válidos")
+    if multichannel:
+        # Não Filtra por Canais
+        if not isinstance(sigma, coll.Iterable):
+            sigma = [sigma] * (image.ndim - 1)
+        if len(sigma) != image.ndim:
+            sigma = np.concatenate((np.asarray(sigma), [0]))
+    image = convert_to_float(image, preserve_range)
+    return ndi.gaussian_filter(image, sigma, mode=mode, cval=cval,
+truncate=truncate)
+
         pass
         
 class Color(Filter):
